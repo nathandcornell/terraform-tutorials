119e69bcb9426927d5d102c69c93782a6a494312
Nate Cornell <nate.cornell@xenplatforms.com>
2023-09-07 16:19:14 -0700

    Add aws-remote-state

---
+++ aws-remote-state/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  cloud {
+    organization = "finvoice"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+# Get current account:
+data "aws_caller_identity" "current" {}
+output "account_id" { value = data.aws_caller_identity.current.account_id }
